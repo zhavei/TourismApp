1e160f07a4fed92e416888c606984da7dadfe952
zhavei <zhavei.vei@gmail.com>
2023-03-07 15:39:54 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismInteractor.kt
@@ -0,0 +1,21 @@
+package com.dicoding.tourismapp.core.domain.usecase
+
+import androidx.lifecycle.LiveData
+import com.dicoding.tourismapp.core.data.Resource
+import com.dicoding.tourismapp.core.domain.model.Tourism
+import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
+import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
+
+class TourismInteractor (private val tourismRepository: ITourismRepository) : TourismUseCase {
+    override fun getAllTourism(): LiveData<Resource<List<Tourism>>> {
+        return tourismRepository.getAllTourism()
+    }
+
+    override fun getFavoriteTourism(): LiveData<List<Tourism>> {
+        return tourismRepository.getFavoriteTourism()
+    }
+
+    override fun setFavoriteTourism(tourism: Tourism, state: Boolean) {
+        return tourismRepository.setFavoriteTourism(tourism, state)
+    }
+}
