0a21411179535f14ead67c8210077bdd70dba66e
zhavei <zhavei.vei@gmail.com>
2023-03-14 21:25:38 +0700

    add

--- app/src/main/java/com/dicoding/tourismapp/MyApplication.kt
+++ app/src/main/java/com/dicoding/tourismapp/MyApplication.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import com.dicoding.tourismapp.core.di.CoreComponent
 import com.dicoding.tourismapp.di.AppComponent
 import com.dicoding.tourismapp.di.DaggerAppComponent
-import com.dicoding.tourismapp.di.DaggerCoreComponent
+import com.dicoding.tourismapp.di.DaggerCoreComponentgit
 
 open class MyApplication : Application() {
 
